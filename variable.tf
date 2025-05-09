da29a74f154b2515a4f87ed131ec40bfbacd0880
ganesh-redy <ganeshreddy7610@gmail.com>
2025-05-09 16:59:08 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "image" {
+    description = "image_of vm"
+  
+}
+variable "machine_type" {
+    description = "give gcp machine type"
+  
+}
+variable "project_id" {
+    description = "project id"
+  
+}
+variable "zone" {
+    description = "zone to create vm"
+  
+}
+variable "name" {
+  description = "name of vm"
+}
+
