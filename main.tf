4df980fca47ce63fde1965ba9c73973326103208
ganesh-redy <ganeshreddy7610@gmail.com>
2025-05-09 18:05:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 provider "google" {
-    zone = "${var.zone}"
-    project = "${var.project_id}"
-  
+  zone    = var.zone
+  project = var.project_id
+}
+
+provider "harness" {
+  endpoint         = "https://app.harness.io/gateway"
+  account_id       = "your_account_id_here"
+  platform_api_key = "<+secrets.getValue(\"project.harness_api_key\")>" # Recommended
 }
 
 terraform {
@@ -18,37 +23,35 @@ resource "tls_private_key" "my_ssh_key" {
 }
 
 resource "google_compute_instance" "instance0" {
-    name = "${var.name}"
-    machine_type = "${var.machine_type}"
-    boot_disk {
-      initialize_params {
-        image = "${var.image}"
-      }
+  name         = var.name
+  machine_type = var.machine_type
+
+  boot_disk {
+    initialize_params {
+      image = var.image
     }
-    network_interface {
-      network = "default"
-      access_config {
-        
-      }
-    }
-    metadata = {
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  metadata = {
     ssh-keys = "ansible:${tls_private_key.my_ssh_key.public_key_openssh}"
   }
-  
 }
 
 output "vm_external_ip" {
-   value = google_compute_instance.instance0.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.instance0.network_interface[0].access_config[0].nat_ip
 }
 
 resource "harness_platform_secret_text" "inline" {
-  identifier  = "private-key-ansible"
-  name        = "private-key-ansible"
-  description = "terraform create the private key and store in the serects of harness"
-  tags        = ["foo:bar"]
-
+  identifier                = "private-key-ansible"
+  name                      = "private-key-ansible"
+  description               = "terraform create the private key and store in the secrets of harness"
+  tags                      = ["foo:bar"]
   secret_manager_identifier = "harnessSecretManager"
   value_type                = "Inline"
   value                     = tls_private_key.my_ssh_key.private_key_pem
-  
 }
